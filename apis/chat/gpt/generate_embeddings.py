4b5300791910f6706d9c1526161488b9451e5b10
Michael Zhu <michael.jx.zhu@gmail.com>
2023-03-14 20:26:27 -0700

    remove old data and add excel

--- apis/chat/gpt/generate_embeddings.py
+++ apis/chat/gpt/generate_embeddings.py
@@ -5,18 +5,29 @@ import os
 from langchain.vectorstores import Pinecone
 from settings import PINECONE_INDEX, PINECONE_INDEX_NAME, PINECONE_TEXT_KEY, OPENAI_EMBEDDINGS
 
-# Get all .txt in current directory
-filenames = []
-for file in os.listdir("data"):
-    if file.endswith(".txt"):
-        filenames.append("data/" + file)
+from openpyxl import load_workbook
 
-# Read in all .txt from file
+# array of texts to ingest
 texts = []
-for file in filenames:
-    file_handle = open(file, 'r')
-    data = file_handle.read()
-    texts.append(data)
+
+# load workbook
+workbook = load_workbook(filename = "data/data.xlsx")
+for worksheet in workbook.worksheets:
+    # for each worksheet, generate tuples of "label" and actual "value"
+    label_value_tuples = []
+    for row_num in range(1, 1000):
+        label_cell = worksheet["A" + str(row_num)]
+        value_cell = worksheet["B" + str(row_num)]
+        if label_cell.value == None or value_cell.value == None:
+            break
+        label_value_tuple = str(label_cell.value) + ": " + str(value_cell.value)
+        label_value_tuples.append(label_value_tuple)
+        print("Generated new tuple: {}".format(label_value_tuple))
+
+    # join the tuples together w/ newlines to form text
+    text = "\n".join(label_value_tuples)
+    texts.append(text)
+    print("Appended new text: {}".format(text))
 
 vectorstore = Pinecone.from_existing_index(
     PINECONE_INDEX_NAME,
