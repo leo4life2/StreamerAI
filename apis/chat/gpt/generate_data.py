5083b9cde5d02a6d31a6dccfb2d5a18564aad5f9
Michael Zhu <michael.jx.zhu@gmail.com>
2023-03-14 20:39:28 -0700

    add data generation

---
+++ apis/chat/gpt/generate_data.py
@@ -0,0 +1,69 @@
+import logging
+import sys
+import os
+
+from langchain.vectorstores import Pinecone
+from settings import PINECONE_INDEX, PINECONE_INDEX_NAME, PINECONE_TEXT_KEY, OPENAI_EMBEDDINGS
+
+from openpyxl import load_workbook
+
+def generate_and_load_embeddings():
+    # array of texts to ingest
+    texts = []
+
+    # load workbook
+    workbook = load_workbook(filename = "data/data.xlsx")
+    for worksheet in workbook.worksheets:
+        # for each worksheet, generate tuples of "label" and actual "value"
+        label_value_tuples = []
+        for row_num in range(1, 1000):
+            label_cell = worksheet["A" + str(row_num)]
+            value_cell = worksheet["B" + str(row_num)]
+            if label_cell.value == None or value_cell.value == None:
+                break
+            label_value_tuple = str(label_cell.value) + ": " + str(value_cell.value)
+            label_value_tuples.append(label_value_tuple)
+            print("Generated new tuple: {}".format(label_value_tuple))
+
+        # join the tuples together w/ newlines to form text
+        text = "\n".join(label_value_tuples)
+        texts.append(text)
+        print("Appended new text: {}".format(text))
+
+    vectorstore = Pinecone.from_existing_index(
+        PINECONE_INDEX_NAME,
+        OPENAI_EMBEDDINGS,
+        PINECONE_TEXT_KEY
+    )
+
+    if len(texts) > 0:
+        vectorstore.add_texts(texts)
+        print("initialized pinecone and added texts")
+    else:
+        print("no texts found")
+
+def generate_and_write_text_files():
+    # load workbook
+    workbook = load_workbook(filename = "data/data.xlsx")
+    for index, worksheet in enumerate(workbook.worksheets):
+        # for each worksheet, generate tuples of "label" and actual "value"
+        label_value_tuples = []
+        for row_num in range(1, 1000):
+            label_cell = worksheet["A" + str(row_num)]
+            value_cell = worksheet["B" + str(row_num)]
+            if label_cell.value == None or value_cell.value == None:
+                break
+            label_value_tuple = str(label_cell.value) + ": " + str(value_cell.value)
+            label_value_tuples.append(label_value_tuple)
+            print("Generated new tuple: {}".format(label_value_tuple))
+
+        file_name = str(index + 1) + ".txt"
+
+        # join the tuples together w/ newlines to form text
+        text = "\n".join(label_value_tuples)
+
+        # write to file
+        file_name = "data/" + str(index + 1) + ".txt"
+        f = open(file_name, "w")
+        f.write(text)
+        print("Wrote to file: {} text: {}".format(file_name, text))
--- apis/chat/gpt/retrieval.py
+++ apis/chat/gpt/retrieval.py
@@ -22,9 +22,28 @@ def retrieve_with_prompt(message):
 
     List of product descriptions:
 
-    1) "日本进口花王马桶去污清洁剂，去污去味，高效洁净不留死角"
-    2) "大王Elleair抽取式面巾纸，三层柔韧持久，湿水不易皱，无屑抗敏感，适合宝宝肉内肌肤，方便携带"
-    3) "安娜美肌牙膏，意大利原装进口，欧洲先锋护理口腔护理品牌，小苏打薄荷香清新口气，慕斯状膏体泡沫绵密。"
+    ```
+    1) "德国双心辅酶q10胶囊ql0心脏保健品中老年成人coq10心脑血管30粒"
+    2) "Schiff 旭福液体钙片补钙维生素 d3 碳酸钙加镁中老年人钙片 90 粒*2"
+    3) "ISDG 日本进口夜间酵素 232 种植物果蔬水果孝素 120 粒/袋*2"
+    4) "biowell 羊胎素胶囊内调保养女性羊胚胎羊胎盘提取物进口第四餐"
+    5) "NYO3护肝片挪威进口140倍奶蓟草小绿盾水飞蓟素加班熬夜应酬保健"
+    6) "NYO3挪威原装进口纯南极磷虾油56%高海洋磷脂鱼油升级omega-3x2瓶"
+    7) "Biowell新加坡进口富硒片 活性有机天然酵母硒元素第4代硒片维e"
+    8) "美国进口viyouth维养思益生菌成年大人益生菌食品调理养胃"
+    9) "MDC小腰精酵素30粒左旋肉碱黑生姜日夜间植物本孝素促进脂肪分解"
+    10) "澳乐维他澳洲原装进口虾青素雨生红球藻胶囊口服精华90粒/瓶"
+    11) "SpringLeaf绿芙澳洲进口黑蜂胶软胶囊高浓度2000mg365粒"
+    12) "俄罗斯蓝胖子羊奶粉"
+    13) "ISDG 日本进口甜蜜习惯抗糖丸 热控片非白芸"
+    14) "德国大马膏原装正品进口七叶庄园马栗按摩凝胶颈肩腰腿损伤修复油"
+    15) "意大利进口小苏打牙膏"
+    16) "日本近江兄弟小熊防晒霜防紫外线女夏季清爽学生军训身体防晒乳男"
+    17) "日本进口花王马桶去污清洁剂"
+    18) "日本大王爱璐儿elleair超软宝宝专用保湿纸巾抽纸婴儿用纸60抽*4"
+    ```
+
+    The returned number must be between 1 and 18.
 
     Customer Question: "Which one should be used for my skin?"
     Product: 1
@@ -35,20 +54,17 @@ def retrieve_with_prompt(message):
     prompt = PromptTemplate(template=template, input_variables=["question"])
     llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
     result = llm_chain.predict(question=message)
+    print("llm returned: {}".format(result))
 
     # llm results are messy, sometimes it's "Answer2", "2", or "2大王Elleair抽取..."
     # let's take only numbers, then take the first number
     cleaned_result = ''.join(c for c in result if c in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])[0:]
-    print(cleaned_result)
+    print("cleaned_result: {}".format(cleaned_result))
+    if cleaned_result == None:
+        return ''
 
-    number_to_filename = {
-        "1": "21.txt",
-        "2": "22.txt",
-        "3": "18.txt"
-    }
-    if cleaned_result in number_to_filename:
-        filename = "./data/" + number_to_filename[cleaned_result]
-        f = open(filename, "r")
-        return f.read()
-
-    return ''
+    filename = "./data/" + str(cleaned_result) + ".txt"
+    f = open(filename, "r")
+    text = f.read()
+    print("returned text: {}".format(text))
+    return text
