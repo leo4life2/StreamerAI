0a6566828dac45e4e44d3c75980d12ff8efc5fe2
Michael Zhu <michael.jx.zhu@gmail.com>
2023-05-03 21:00:48 -0700

    save script

--- StreamerAI/database/database.py
+++ StreamerAI/database/database.py
@@ -6,23 +6,75 @@ class StreamCommentsDB:
         cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS stream_comments (
-                id INTEGER PRIMARY KEY,
-                stream_identifier TEXT NOT NULL,
-                username TEXT NOT NULL,
-                comment TEXT NOT NULL,
-                read INTEGER NOT NULL DEFAULT FALSE
+                id INTEGER PRIMARY KEY, /* unique id primary key */
+                stream_identifier TEXT NOT NULL, /* unique identifier that represents the stream, likely the streaming platform's channel id */
+                username TEXT NOT NULL, /* username of the commenter */
+                comment TEXT NOT NULL, /* text of the comment */
+                read INTEGER NOT NULL DEFAULT FALSE /* whether the comment has been processed by the assistant or not */
             );
             """
         )
         cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS stream_cursor (
-                stream_identifier TEXT PRIMARY KEY,
-                cursor TEXT NOT NULL
+                stream_identifier TEXT PRIMARY KEY, /* unique identifier that represents the stream, likely the streaming platform's channel id */
+                cursor TEXT NOT NULL /* the stream comment cursor */
             );
             """
         )
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS products (
+                name TEXT PRIMARY KEY, /* name of the product */
+                description TEXT NOT NULL /* description of the product, FAQs, etc. will be directly injected into the prompt *.
+            )
+            """
+            # we can consider separating description into multiple columns - get this working for now
+        )
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS assistants (
+                name TEXT PRIMARY KEY, /* name of the assistant profile */
+                prompt TEXT NOT NULL /* a description/command of how the assistant should behave. this will be directly injected into the prompt */
+            )
+            """
+        )
         connection.commit()
+
+    @staticmethod
+    def add_product(connection, name, description):
+        """Add a new product"""
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO products (name, description) VALUES (?, ?)", (name, description))
+        cursor.commit()
+    
+    @staticmethod
+    def query_all_products(connection):
+        """Returns all registered products"""
+        cursor = connection.cursor()
+        cursor.execute("SELECT name, description FROM products")
+        return cursor.fetchall()
+
+    @staticmethod
+    def remove_product(connection, name):
+        """Remove the product with the specified name"""
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM products WHERE name = ?", (name,))
+        cursor.commit()
+
+    @staticmethod
+    def add_assistant(connection, name, prompt):
+        """Add a new assistant profile"""
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO assistants (name, prompt) VALUES (?, ?)", (name, prompt))
+        connection.commit()
+
+    @staticmethod
+    def get_prompt_for_assistant(connection, name):
+        """Given an assistant name, fetches the prompt that should be used"""
+        cursor = connection.cursor()
+        cursor.execute("SELECT prompt FROM assistants WHERE name = ?", (name,))
+        return cursor.fetchall()
 
     @staticmethod
     def add_comment(connection, stream_identifier, username, comment):
---
+++ StreamerAI/streaming/display_images.py
@@ -0,0 +1,21 @@
+import cv2
+
+img1 = cv2.imread('test1.jpg')
+cv2.imshow('test', img1)
+cv2.waitKey(1)
+
+img2 = cv2.imread('test2.jpg')
+cv2.imshow('test', img2)
+cv2.waitKey(1)
+
+cv2.destroyAllWindows()
+
+"""
+- OBS has built in settings to display images, scroll them, etc.. https://www.youtube.com/watch?v=npSLOVsZ9yQ
+
+- streamelements is kind of an "all in one" for overlays https://streamelements.com/features/overlays 
+- works in conjunction with OBS
+
+
+"""
+
